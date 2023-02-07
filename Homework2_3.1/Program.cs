bdc118847e340e9c79a4ac154bd78e5bd130ecf3
RiokaDino <110732907+RiokaDino@users.noreply.github.com>
2023-02-07 23:56:27 +0700

    Week2_3.1

---
+++ Homework2_3.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Input number: ");
+        double x = double.Parse(Console.ReadLine());
+        Console.WriteLine(Function(x));
+    }
+    static double Function(double x)
+    {
+        if(x > -1 && x <= 0)
+        {
+            return Math.Sqrt(1-Math.Pow(x , 2));
+        }
+        if(x > 0 && x <= 1)
+        {
+            return -1*Math.Sqrt(1-Math.Pow(x , 2)); 
+        }
+        if(x <= -1)
+        {
+            return Function(x + 2);
+        }
+        else
+        {
+            return Function(x - 2);
+        }
+    }
+}
